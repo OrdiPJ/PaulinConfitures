14bf87c0c315a4d636f32c68dd1b58a36c872dc9
OrdiPJ <paulin.juhen@outlook.com>
2021-04-18 18:11:01 +0200

    Premiers codes

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>Acceuil - Paulin Confitures</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/logo.jpg">
+</head>
+
+<body>
+    <div id="carte">
+        <header>
+            <h1>Paulin Confitures</h1>
+        </header>
+        <?php include 'menu.php'; ?>
+        <h2>Bienvenue sur le site de Paulin Confitures</h2>
+        <p>Ici, vous pourez :<br>
+        - Commander nos confitures,<br>
+        - Voir nos produits, <br>
+        - Et tout apprendre ce qui concerne l'usine.</p>
+        <h3>Je vous laisse découvrir le site !!!</h3>
+        <footer>
+            <p>Copyright &copy; Paulin Confitures - 2020-2021 - Tous Droits Réservés</p>
+        </footer>
+    </div>
+</body>
+</html>
